b5b4b2c3c83e53f6cdabcd517b72e0ed52297495
eduardo <edux018@gmail.com>
2022-02-18 10:23:23 -0300

    SplashScreen fix

--- app/src/main/java/com/example/vacation_manager_android/MainActivity.kt
+++ app/src/main/java/com/example/vacation_manager_android/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.vacation_manager_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-        Thread.sleep(2000)
-        setTheme(R.style.Theme_VacationManagerAndroid)
+    override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/vacation_manager_android/SplashScreen.kt
+++ app/src/main/java/com/example/vacation_manager_android/SplashScreen.kt
@@ -1,11 +1,24 @@
 package com.example.vacation_manager_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SplashScreen : AppCompatActivity() {
+    lateinit var iv_notes: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        iv_notes = findViewById(R.id.iv_note)
+
+        iv_notes.alpha = 0f
+        iv_notes.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
     }
 }
