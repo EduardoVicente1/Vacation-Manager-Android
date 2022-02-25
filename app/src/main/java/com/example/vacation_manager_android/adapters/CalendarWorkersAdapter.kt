e836fc87ff13bf09a80f1962349009c605c4cdda
Marvid21 <arguellomarcelo48@gmail.com>
2022-02-24 22:36:19 -0300

    Add colors Team

--- app/src/main/java/com/example/vacation_manager_android/adapters/CalendarWorkersAdapter.kt
+++ app/src/main/java/com/example/vacation_manager_android/adapters/CalendarWorkersAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vacation_manager_android.R
 import com.example.vacation_manager_android.data_classes.WorkersGetResponse
@@ -18,28 +19,29 @@ class CalendarWorkersAdapter(var onVacationWorkersList: List<WorkersGetResponse.
         fun bind(onWorkerElement: WorkersGetResponse.Data?){
             workerName.text = onWorkerElement?.attributes?.workerName.toString()
             var checkonVacation=onWorkerElement?.attributes?.onVacation
-            workerTeam.text = "GNB"
-//            workerTeam.text = onWorkerElement?.attributes?.workTeam.toString()
+            workerTeam.text = onWorkerElement?.attributes?.workTeam.toString()
             imgcolor=view.findViewById(R.id.img_calendar_worker_color)
 
-            when(workerTeam.text){
-                "Itau Fabrica" -> imgcolor.setColorFilter(R.color.itau_fabrica)
-                "Itau Paseo" -> imgcolor.setColorFilter(R.color.itau_paseo)
-                "Familiar" -> imgcolor.setColorFilter(R.color.familiar)
-                "Atlas" -> imgcolor.setColorFilter(R.color.atlas)
-                "GNB" -> imgcolor.setColorFilter(R.color.gnb)
-                "Sudameris" -> imgcolor.setColorFilter(R.color.sudameris)
-                "RoshkaStudios" -> imgcolor.setColorFilter(R.color.roshka_studios)
-                "Administracion" -> imgcolor.setColorFilter(R.color.administracion)
-                "Diseñadores" -> imgcolor.setColorFilter(R.color.diseñadores)
-                "Operaciones" -> imgcolor.setColorFilter(R.color.operaciones)
-                "QA" -> imgcolor.setColorFilter(R.color.qa)
-                "Soporte" -> imgcolor.setColorFilter(R.color.soporte)
-                "Squadra" -> imgcolor.setColorFilter(R.color.squadra)
-                "Sysadmin" -> imgcolor.setColorFilter(R.color.sysadmin)
-                "Tatakua Team" -> imgcolor.setColorFilter(R.color.tatakua_team)
-                "TH" -> imgcolor.setColorFilter(R.color.th)
-                "Victor Villalba" -> imgcolor.setColorFilter(R.color.victor_villalba)
+            when(workerTeam.text.toString()){
+                "Itau Fabrica" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.itau_fabrica))
+                "Itau Paseo" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.itau_paseo))
+                "Familiar" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.familiar))
+                "Atlas" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.atlas))
+                "GNB" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.gnb))
+                "Sudameris" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.sudameris))
+                "RoshkaStudios" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.roshka_studios))
+                "Administracion" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.administracion))
+                "Diseñadores" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.diseñadores))
+                "Operaciones" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.operaciones))
+                "QA" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.qa))
+                "Soporte" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.soporte))
+                "Squadra" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.squadra))
+                "Sysadmin" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.sysadmin))
+                "Tatakua Team" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.tatakua_team))
+                "TH" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.th))
+                "Victor Villalba" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.victor_villalba))
+                "Walberto Team" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.diseñadores))
+                "Manchester United 2" -> imgcolor.setBackgroundColor(getColor(view.context,R.color.soporte))
             }
         }
 
